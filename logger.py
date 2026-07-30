5ab2555c6bdf6ae216a56281a7647eb63cf65022
nasty64759-cpu <nasty64759@gmail.com>
2026-07-30 18:22:59 +0300

    Update logger.py

--- logger.py
+++ logger.py
@@ -1,1 +1,23 @@
-n
+from loguru import logger
+import sys
+
+
+logger.remove()
+
+logger.add(
+    sys.stdout,
+    level="INFO",
+    colorize=True,
+    format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> | "
+           "<level>{level: <8}</level> | "
+           "<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> | "
+           "<level>{message}</level>"
+)
+
+logger.add(
+    "hyper_trader.log",
+    rotation="10 MB",
+    retention="30 days",
+    compression="zip",
+    level="INFO"
+)
